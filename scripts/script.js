d78914da4e28b520ea688c1cccb02b49c383a93e
divperedi <132815746+divperedi@users.noreply.github.com>
2024-02-11 16:02:10 +0100

    game works, facing issues with logging in(game starts even without logging in)

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,23 @@
 'use strict';
 
+window.addEventListener('load', () => {
+    document.querySelector('#create').addEventListener('click', registerUser);
+    document.querySelector('#play').addEventListener('click', validateLogin);
+    document.querySelectorAll('#loginToggle, #registrationToggle').forEach(btn => btn.addEventListener('click', toggleForms));
+    gameArea.style.display = 'none';
+    logo.style.display = 'none';
+    instructionText.style.display = 'none';
+});
+
+import { users } from './users.js';
+
 const board = document.querySelector('#game-board');
 const instructionText = document.querySelector('#instruction-text');
 const logo = document.querySelector('#logo');
 const score = document.querySelector('#score');
 const highScoreText = document.querySelector('#highScore');
+let currentUser = {};
+// const loginMsg = document.querySelector('#loginMsg');
 
 const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
@@ -14,6 +27,127 @@ let direction = 'right';
 let gameInterval;
 let gameSpeedDelay = 300;
 let gameStarted = false;
+let formWrapper = document.querySelector('#formWrapper');
+let formRegistration = document.querySelector('#formRegistration');
+let gameArea = document.querySelector('#gameArea');
+
+function getUsers() {
+    if (!localStorage.getItem('users')) {
+        localStorage.setItem('users', JSON.stringify(users));
+    }
+
+    return JSON.parse(localStorage.getItem('users'));
+
+}
+
+function addUser(username, password) {
+    localStorage.setItem(
+        'users',
+        JSON.stringify([
+            ...getUsers(),
+            {
+                name: username,
+                password: password,
+                id: getUsers()[getUsers().length - 1]?.id + 1 || 1,
+            },
+        ])
+    );
+}
+
+function setCurrentUser(userId) {
+    currentUser =
+        getUsers().filter((user) => user.id === userId)[0] || {};
+}
+
+function toggleForms(event) {
+    if (event) {
+        event.preventDefault();
+    }
+
+    document.querySelectorAll('#formLogin, #formRegistration').forEach(div => div.classList.toggle('main__form--hidden'));
+}
+
+function registerUser(event) {
+    event.preventDefault();
+    const message = document.querySelector('#registrationMsg');
+
+    const username = document.querySelector('#registerUsername').value;
+    const password = document.querySelector('#registerPassword').value;
+    const passwordAgain = document.querySelector('#registerPasswordAgain').value;
+
+    if (username.length === 0 || password.length === 0 || passwordAgain.length === 0) {
+        message.textContent = 'You must fill in all information!';
+    }
+    else {
+        // console.log(getUsers());
+        if (getUsers().some((user) => user.name === username)) {
+            message.textContent = 'The user already exists!';
+        } else if (password !== passwordAgain) {
+            message.textContent = 'The password does not match!';
+        } else {
+            addUser(username, password);
+
+            document.querySelector('#loginMsg').textContent = 'User created!';
+            toggleForms();
+        }
+
+    }
+}
+
+function toggleFormDivs() {
+    document.querySelector('#formWrapper').classList.toggle('main__form-wrapper--hidden');
+}
+
+function validateLogin(event) {
+    event.preventDefault();
+    formWrapper.style.display = 'none';
+    formRegistration.style.display = 'none';
+    gameArea.style.display = 'block';
+    logo.style.display = 'block';
+    instructionText.style.display = 'block';
+    // startGame();
+    
+    const userName = document.querySelector('#loginUsername').value; 
+    const passWord = document.querySelector('#loginPassword').value; 
+    const question = document.querySelector('#question').checked; 
+
+    try {
+        if (userName === '') {
+            throw {
+                'nodeRef': document.querySelector('#loginUsername'), 
+                'msg': 'Username is required!'
+            };
+        }
+        if (passWord === '') {
+            throw {
+                'nodeRef': document.querySelector('#loginPassword'), 
+                'msg': 'Password is required!' 
+            };
+        }
+        if (!question) {
+            throw {
+                'nodeRef': document.querySelector('#question'), 
+                'msg': 'Confirm that you are ready to go back to 90s!' 
+            };
+        }
+        const users = getUsers();
+        const user = users.find(user => user.name === userName && user.password === passWord); 
+        if (!user) {
+            throw {
+                'nodeRef': document.querySelector('#loginUsername'),
+                'msg': 'Invalid username or password!' 
+            };
+        }
+        currentUser = user;
+        startGame();
+        return true; 
+
+
+    } catch (error) {
+        document.querySelector('#loginMsg').textContent = error.msg; 
+        return false; 
+    }
+}
 
 function draw() {
     board.innerHTML = '';
@@ -98,9 +232,12 @@ function move() {
 // }, 200)
 
 function startGame() {
+    toggleFormDivs();
     gameStarted = true;
     instructionText.style.display = 'none';
     logo.style.display = 'none';
+    formWrapper.style.display = 'none';
+    formRegistration.style.display = 'none';
     gameInterval = setInterval(() => {
         move();
         checkCollision();
@@ -173,6 +310,7 @@ function resetGame() {
 function updateScore() {
     const currentScore = snake.length - 1;
     score.textContent = currentScore.toString().padStart(3, '0');
+    // loginMsg.textContent = `${currentUser.name} you ate ` + currentScore + ` pieces!`;
 }
 
 function stopGame() {
@@ -180,6 +318,22 @@ function stopGame() {
     gameStarted = false;
     instructionText.style.display = 'block';
     logo.style.display = 'block';
+    clearGameBoard();
+}
+
+
+
+function clearGameBoard() {
+    const snakeElements = document.querySelectorAll('.snake');
+    const foodElement = document.querySelector('.food');
+
+    snakeElements.forEach(element => {
+        element.remove();
+    });
+
+    if (foodElement) {
+        foodElement.remove();
+    }
 }
 
 function updateHighScore() {
